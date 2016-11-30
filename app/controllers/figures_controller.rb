c30c54aa1071ce584f84ee28098aff04588acd92
neener <janinemrosen@gmail.com>
2016-11-30 17:59:26 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,6 +11,12 @@ class FiguresController < ApplicationController
 
 	get '/figures/:id' do
 		@figure = Figure.find(params[:id])
+		erb :'figures/show'
+	end
+
+	get '/figure/:id/edit' do
+		@figure = Figure.find(params[:id])
 		erb :'figures/edit'
+	end
 
 end
